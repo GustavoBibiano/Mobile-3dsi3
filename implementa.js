e0fc0edab1aec474b81bc3958df343a590f58cbb
GustavoBibiano <gu.bibiano@hotmail.com>
2019-11-29 23:15:44 +0000

    Primeiro Commit

--- implementa.js
+++ implementa.js
@@ -1,5 +1,10 @@
 ///<reference path="pessoa.ts"/>
 var empresa;
 (function (empresa) {
+    //instancia de classe
     var p = new empresa.Pessoa();
+    p.setNome("Gustavo");
+    p.setIdade(18);
+    p.setCpf("000.000.000-00");
+    document.getElementById("nome").textContent = p.getNome();
 })(empresa || (empresa = {}));
