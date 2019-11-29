07eb906e3758b473239abf4bd7e8300b6da756d3
Gustavo Bibiano Lopes <gu.bibiano@hotmail.com>
2019-11-29 23:06:44 +0000

    Versão inicial

---
+++ implementa.js
@@ -0,0 +1,5 @@
+///<reference path="pessoa.ts"/>
+var empresa;
+(function (empresa) {
+    var p = new empresa.Pessoa();
+})(empresa || (empresa = {}));
---
+++ implementa.ts
@@ -0,0 +1,4 @@
+///<reference path="pessoa.ts"/>
+namespace empresa{
+    let p = new Pessoa();
+}
